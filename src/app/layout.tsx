de2d37fe1104ae0d732c5a5e582a9d43aaa2cc57
csmaxi <csmaxinro@gmail.com>
2024-01-13 20:36:23 -0300

    ana

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Mini-Cuentos',
@@ -15,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
-      
+      <Analytics />
     </html>
   )
 }
