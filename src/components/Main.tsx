36e7f02bfc78b2bc2c9ec90148628e4a46c201a8
csmaxi <csmaxinro@gmail.com>
2024-01-13 13:57:17 -0300

    cafecito

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-
 export default function Component() {
   return (
     <section
@@ -18,15 +17,15 @@ export default function Component() {
             className="inline-flex h-12 items-center justify-center rounded-md bg-gray-700 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
             href="https://cafecito.app/csmaxi" target="_blank"
           >
-            <h2 className="text-xl">Invitame un Cafecito</h2>
+            <h2 className="text-xl">Cafecito</h2>
           </Link>
         </div>
       </div>
       <div className="absolute top-4 right-4 flex gap-2">
         
         <Link href="https://www.youtube.com/@minicuentosapp" target="_blank">
-            <Button className=" border-white"  variant="outline">
-              Youtube
+            <Button className=" border-white"  variant="destructive">
+              YouTube
             </Button>
         </Link>
       </div>
