a74e473ddb595027396920d28be3d3f136a1a750
csmaxi <csmaxinro@gmail.com>
2024-01-24 18:28:33 -0300

    blancanieves

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,9 @@
+function TypographyH1() {
+    return (
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Blancanieves
+    </h1>
+    );
+}
+
+export default TypographyH1;
