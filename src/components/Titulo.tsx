5fc031cae9da36dd1d6f7719f73985862245c8a8
csmaxi <csmaxinro@gmail.com>
2024-01-24 18:48:15 -0300

    nombres

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,9 +1,10 @@
-function TypographyH1() {
-    return (
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      Blancanieves
-    </h1>
-    );
+
+interface TypographyH1Props {
+  texto: string;
+}
+
+const TypographyH1: React.FC<TypographyH1Props> = ({ texto }) => {
+  return <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{texto}</h1>;
 }
 
 export default TypographyH1;
