0124043611dbeb999288907b35a4e3e9de9f7dc9
csmaxi <csmaxinro@gmail.com>
2024-07-16 05:02:12 -0300

    primer commit

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import { quicksand, fredoca, gochi } from "./fonts"
-import { Cuentos } from "../../data/datos"
-import { motion } from "framer-motion"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { quicksand, fredoca, gochi } from "./fonts";
+import { Cuentos } from "../../data/datos";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 function Cards() {
-
-    return (
-
-        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-5 lg:m-2 lg:p-2 ">
-            
-            {Cuentos.map((cuento) => (
-
-                <Card key={cuento.id} className="flex flex-col justify-between border-2">
-                    <CardHeader className="flex-row gap-4 items-center justify-around">
-                        <Avatar className="ring ring-offset-2 ring-slate-800" >
+  return (
+    <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-5 lg:m-2 lg:container lg:mx-auto ">
+      {Cuentos.map((cuento) => (
+        <Card
+          key={cuento.id}
+          className="flex flex-col justify-between border-8 shadow border-border"
+        >
+          <CardHeader className="flex-row gap-4 items-center justify-around">
+            {/* <Avatar className="ring ring-offset-2 ring-slate-800" >
                             <AvatarImage
                                 src={`/image/${cuento.id}.jpg`}
                                 alt={cuento.title}
@@ -33,32 +33,60 @@ function Cards() {
                             <AvatarFallback>
                                 {cuento.title.slice(0, 2)}
                             </AvatarFallback>
-                        </Avatar>
-                        <CardTitle className={`${quicksand.className} antialiased text-center`}>{cuento.title}</CardTitle>
-                        <CardDescription>3'</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p className={`${fredoca.className} antialiased text-center p-4 text-lg`}>{cuento.description}</p>
-                    </CardContent>
-                    <CardFooter className="flex justify-center gap-2">
-                        <motion.div
-                            whileHover={{ scale: 1.1 }}
-                            className={`${gochi.className} antialiased text-center `}>
-                            <Link href={cuento.cuentoen}>
-                                <Button variant={"outline"} className="hover:text-red-600 text-xl">Inglés</Button>
-                            </Link>
+                        </Avatar> */}
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <CardTitle
+                className={`${quicksand.className} antialiased text-center cursor-grab border-8 shadow p-2 rounded-lg border-border text-blue-700`}
+              >
+                {cuento.title}
+              </CardTitle>
+            </motion.div>
 
-                        </motion.div>
-                        <motion.div whileHover={{ scale: 1.1 }}>
-                            <Link href={cuento.cuentoes}>
-                                <Button variant={"outline"} className="hover:text-blue-600 text-xl" >Español</Button>
-                            </Link>
-                        </motion.div>
-                    </CardFooter>
-                </Card>
-            ))}
-        </div>
-    );
+            {/* <CardDescription>3'</CardDescription> */}
+          </CardHeader>
+
+          <CardContent className="flex justify-center">
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <Image
+                src={`/image/${cuento.id}.jpg`}
+                alt={cuento.title}
+                width={300}
+                height={300}
+                className="rounded-full my-3  border-8 hover:border-blue-400 cursor-grabbing"
+              />
+            </motion.div>
+
+            {/* <p className={`${fredoca.className} antialiased text-center p-4 text-lg`}>{cuento.description}</p> */}
+          </CardContent>
+          <CardFooter className="flex justify-center gap-2">
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              className={`${gochi.className} antialiased text-center `}
+            >
+              <Link href={cuento.cuentoen}>
+                <Button
+                  variant={"outline"}
+                  className="hover:text-red-600 text-xl border-2 border-red-200 shadow"
+                >
+                  Inglés
+                </Button>
+              </Link>
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <Link href={cuento.cuentoes}>
+                <Button
+                  variant={"outline"}
+                  className="hover:text-blue-600 text-xl border-2 border-blue-200 shadow"
+                >
+                  Español
+                </Button>
+              </Link>
+            </motion.div>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
 export default Cards;
